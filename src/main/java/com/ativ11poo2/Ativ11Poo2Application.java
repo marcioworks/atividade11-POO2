5ce935972785cb592b9737e610ee24cacd443b81
marcio santos <marcioadsworks@gmail.com>
2021-05-29 14:33:58 -0300

    project started

---
+++ src/main/java/com/ativ11poo2/Ativ11Poo2Application.java
@@ -0,0 +1,13 @@
+package com.ativ11poo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ativ11Poo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ativ11Poo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ativ11poo2/Ativ11Poo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ativ11poo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ativ11Poo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
